e4ae88fec4c6553156419f4b59662370f465159f
Folorunsho Solomon Opeyemi <solomonope@gmail.com>
2013-09-23 20:45:45 +0100

    update

---
+++ src/org/solomon/drivers/BubbleSortDriver.java
@@ -0,0 +1,18 @@
+package org.solomon.drivers;
+
+import org.solomon.ds.*;
+public class BubbleSortDriver {
+
+	public static void main(String [] a){
+	int [] sort = {7, 4, 5, 2, 9, 1};
+	
+	BubbleSort<Object> st = new BubbleSort<Object>(sort);
+	
+	int [] returnsort = st.Sort();
+	
+	for(int i=0; i<returnsort.length-1;i++){
+		System.out.println(returnsort[i]);
+	}
+	
+	}
+}
--- src/org/solomon/ds/BubbleSort.java
+++ src/org/solomon/ds/BubbleSort.java
@@ -1,4 +1,33 @@
 package org.solomon.ds;
-public class BubbleSort {
+public class BubbleSort<T> {
+	
+	int [] m_Data;
+	
+	public BubbleSort(int [] _Data){
+		
+		m_Data = _Data;
+	}
+	
+	public int [] Sort(){
+		int length =  m_Data.length-1;
+		boolean swap = false;
+		do{
+			swap = false;
+			for(int i = 0; i<length; i++){
+				
+				if(m_Data[i] > m_Data[i+1]){
+					
+					int temp = m_Data[i];
+					m_Data[i] = m_Data[i+1];
+					m_Data[i+1] = temp;
+					swap = true;
+				}
+			}
+			
+		}while(swap);
+		
+		return m_Data;
+		
+	}
 
 }
