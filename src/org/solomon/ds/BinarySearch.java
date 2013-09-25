435c4728c621c2140bee2ef831e5e95adb0915eb
Folorunsho Solomon Opeyemi <solomonope@gmail.com>
2013-09-25 02:12:00 +0100

    update

--- src/org/solomon/ds/BinarySearch.java
+++ src/org/solomon/ds/BinarySearch.java
@@ -1,5 +1,36 @@
 package org.solomon.ds;
 public class BinarySearch {
 
+	private int [] m_Data;
 	
+	private int m_Begining;
+	
+	private int m_Middle;
+	
+	private int m_End;
+	
+	public BinarySearch(int [] Data){
+		 m_Data = Data;
+	}
+	
+	
+	public int  Search( int value){
+		
+		return this.Search(value, 0, m_Data.length);
+	}
+	
+	private int Search(int target,int begin,int end){
+		
+		
+		if(begin > end)
+			return -1;
+		
+		int middle = (begin + end) /2;
+		
+		int middlevalue = m_Data[middle];
+		
+		if (middlevalue > target) { return Search(target, begin, middle-1); }
+		if (middlevalue < target) { return Search(target, middle+1, end); }
+		  return middle; //found!
+	}
 }
