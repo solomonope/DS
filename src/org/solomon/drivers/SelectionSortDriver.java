aff1bf3d8197ba34ab2c937cf0c9a968c81fbcf0
Folorunsho Solomon Opeyemi <solomonope@gmail.com>
2013-09-28 18:55:58 +0100

    update

---
+++ src/org/solomon/drivers/SelectionSortDriver.java
@@ -0,0 +1,21 @@
+package org.solomon.drivers;
+import org.solomon.ds.*;
+
+public class SelectionSortDriver {
+	
+	
+	public static void main(String [] a)
+	{
+	int [] sort = {7,7, 4,4, 5, 2,2, 9, 1};
+	
+	SelectionSort st = new SelectionSort(sort);
+	
+	int [] returnsort = st.Sort();
+	
+	for(int i=0; i<returnsort.length-1;i++){
+		System.out.println(returnsort[i]);
+	}
+
+}
+
+}
--- src/org/solomon/ds/SelectionSort.java
+++ src/org/solomon/ds/SelectionSort.java
@@ -2,4 +2,44 @@ package org.solomon.ds;
 
 public class SelectionSort {
 
+	int [] m_Data;
+	
+	public SelectionSort(int [] _Data){
+		
+		m_Data = _Data;
+	}
+	
+	public int [] Sort(){
+		
+		for(int i = 0; i< m_Data.length-1; i++){
+			
+			int _Temp = m_Data[i];
+			
+			int _Min = this.Min(i+1, m_Data.length);
+			
+			int intd = m_Data[_Min];
+			
+			m_Data[i]  = intd;
+			
+			m_Data[_Min] = _Temp;
+			
+			
+		}
+		
+		return m_Data;
+	}
+	
+	private int Min(int _Start, int _End){
+		
+		int Min = _Start;
+		
+		for(int i = _Start; i< _End; i++){
+			
+			if(m_Data[Min] > m_Data[i]){
+				
+				Min = i;
+			}
+		}
+		return Min;
+				}
 }
